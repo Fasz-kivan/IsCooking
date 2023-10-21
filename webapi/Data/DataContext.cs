c76e6847a7bcc4e471b8723362b65d4ac600192d
Ákos Lovász <akos.lovasz@chameleon.sh>
2023-10-21 20:33:55 +0200

    started to lay out entity framework

---
+++ webapi/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapi.Entities;
+
+namespace webapi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Ingredient> Ingredients { get; set; }
+
+        public DbSet<Recipe> Recipes { get; set; }
+
+        public DbSet<Step> Steps { get; set; }
+    }
+}
---
+++ webapi/Entities/Recipe.cs
@@ -0,0 +1,43 @@
+﻿using webapi.NewFolder;
+
+namespace webapi.Entities
+{
+    public class Recipe
+    {
+        public Recipe(
+            int id,
+            string name,
+            string? description,
+            Difficulty difficulty,
+            int minutesToMake,
+            IReadOnlyCollection<Ingredient> ingredients,
+            IReadOnlyCollection<Step> steps,
+            string imageRoute)
+        {
+            Id = id;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Description = description;
+            Difficulty = difficulty;
+            MinutesToMake = minutesToMake;
+            Ingredients = ingredients ?? throw new ArgumentNullException(nameof(ingredients));
+            Steps = steps ?? throw new ArgumentNullException(nameof(steps));
+            ImageRoute = imageRoute ?? throw new ArgumentNullException(nameof(imageRoute));
+        }
+
+        public int Id { get; }
+
+        public string Name { get; }
+
+        public string? Description { get; }
+
+        public Difficulty Difficulty { get; }
+
+        public int MinutesToMake { get; }
+
+        public IReadOnlyCollection<Ingredient> Ingredients { get; }
+
+        public IReadOnlyCollection<Step> Steps { get; }
+
+        public string ImageRoute { get; }
+    }
+}
