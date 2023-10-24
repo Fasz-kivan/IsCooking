bb97ff469d05dda3f3b07fe43a810f6058215065
Ákos Lovász <akos.lovasz@chameleon.sh>
2023-10-24 16:23:17 +0200

    attempt to fill recipe cache

--- webapi/Services/RecipeService.cs
+++ webapi/Services/RecipeService.cs
@@ -1,14 +1,32 @@
-﻿using webapi.Entities;
+﻿using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.Data.Sqlite;
+using webapi.Dtos;
+using webapi.Entities;
 
 namespace webapi.Services
 {
     public class RecipeService
     {
-        private readonly List<Recipe> recipes = new(); // store the db cache here
+        private readonly List<Recipe>? recipes = new(); // store the db cache here
 
-        public RecipeService() 
+        public RecipeService(string connectionString = "Data source = isCooking.db") // todo inherit project db path
         {
-            // todo init, read db to memory
+            using SqliteConnection connection = new(connectionString);
+            connection.Open();
+
+            const string sql = "SELECT * FROM Recipe";
+
+            var cmd = new SqliteCommand(sql, connection);
+
+            var reader = cmd.ExecuteReader();
+
+            foreach (var item in reader)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            //this.recipes = (List<Recipe>)cmd.ExecuteScalar(); // todo fill cache, how tf tho
+            connection.Close();
         }
 
         public void SaveRecipe(Recipe recipe)
@@ -21,9 +39,20 @@ namespace webapi.Services
             return null; // todo
         }
 
-        public IReadOnlyCollection<Recipe> GetAllRecipes()
+        public IReadOnlyCollection<RecipeDto>? GetAllRecipes()
         {
-            return new List<Recipe>(); // todo, but the pattern will be return empty list if not found for instance in filtered gets like getbyingredient
+            if (this.recipes != null)
+            { 
+                IReadOnlyCollection<RecipeDto>? result = this.recipes.Select(x =>
+                {
+                    return new RecipeDto(x.Id, x.Name, x.Description, x.Difficulty, x.MinutesToMake, x.Ingredients, x.Steps, x.ImageRoute);
+                }).ToList(); // todo maybe dont pass internal types in dto, make dto versions of step and ingredient too?
+
+                return result;
+            }
+
+            //the pattern will be return empty list if not found for instance in filtered gets like getbyingredient
+            return new List<RecipeDto>();
         }
     }
 }
